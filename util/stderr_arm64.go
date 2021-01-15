a611af363fd4b8ad3c1d30d264a729ce95538eea
李宇翔 <11123448@vivo.xyz>
2021-01-15 20:54:12 +0800

    develop

--- util/stderr_arm64.go
+++ util/stderr_arm64.go
@@ -15,5 +15,5 @@ func init() {
 		return
 	}
 	// 将进程标准出错重定向至文件，进程崩溃时运行时将向该文件记录协程调用栈信息
-	syscall.Dup2(int(logFile.Fd()), int(os.Stderr.Fd()))
+	syscall.Dup3(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
 }
