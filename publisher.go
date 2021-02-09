c0f821e56e20c3faf2b0082175e6021d8f25c126
langhuihui <178529795@qq.com>
2021-02-09 21:43:18 +0800

    publisher增加Dispose方法

--- publisher.go
+++ publisher.go
@@ -10,14 +10,24 @@ type Publisher struct {
 	context.Context
 	cancel        context.CancelFunc
 	AutoUnPublish bool //	当无人订阅时自动停止发布
-	*Stream `json:"-"` 
-	Type           string //类型，用来区分不同的发布者
+	*Stream       `json:"-"`
+	Type          string //类型，用来区分不同的发布者
 }
 
 // Close 关闭发布者
 func (p *Publisher) Close() {
 	if p.Running() {
 		p.Stream.Close()
+	}
+}
+
+// Dispose 释放RingBuffer的锁，防止订阅者一直阻塞读取
+func (p *Publisher) Dispose() {
+	for _, vt := range p.Stream.VideoTracks {
+		vt.Buffer.Current.Done()
+	}
+	for _, at := range p.Stream.AudioTracks {
+		at.Buffer.Current.Done()
 	}
 }
 
@@ -37,6 +47,6 @@ func (p *Publisher) Publish(streamPath string) bool {
 	p.Publisher = p
 	p.StartTime = time.Now()
 	//触发钩子
-	TriggerHook(Hook{"Publish",p.Stream})
+	TriggerHook(Hook{"Publish", p.Stream})
 	return true
 }
