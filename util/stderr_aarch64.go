13fe0c9e0326dbc449c2fabbfef31dcb8c6f9d0e
jamiesun <jamiesun.net@gmail.com>
2021-03-26 19:06:19 +0800

    Create stderr_aarch64.go
    stderr_aarch64 添加树莓派编译支持

---
+++ util/stderr_aarch64.go
@@ -0,0 +1,19 @@
+// +build linux darwin
+
+package util
+
+import (
+	"log"
+	"os"
+	"syscall"
+)
+
+func init() {
+	logFile, err := os.OpenFile("./fatal.log", os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0666)
+	if err != nil {
+		log.Println("服务启动出错", "打开异常日志文件失败", err)
+		return
+	}
+	// 将进程标准出错重定向至文件，进程崩溃时运行时将向该文件记录协程调用栈信息
+	syscall.Dup3(int(logFile.Fd()), int(os.Stderr.Fd()), 0)
+}
