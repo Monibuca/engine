bd2431921be99f495ac96784da46a647dc75a45a
dexter <178529795@qq.com>
2022-10-05 16:44:10 +0800

    🐛 FIX: keepalive

--- config/remote.go
+++ config/remote.go
@@ -25,51 +25,46 @@ func (cfg *Engine) Remote(ctx context.Context) error {
 		NextProtos:         []string{"monibuca"},
 	}
 
-	conn, err := quic.DialAddr(cfg.Server, tlsConf, nil)
-	if err != nil {
-		log.Error("connect to console server ", cfg.Server, err)
-		return err
-	}
+	conn, err := quic.DialAddr(cfg.Server, tlsConf, &quic.Config{
+		KeepAlive: true,
+	})
 
-	stream, err := conn.OpenStreamSync(ctx)
-	if err != nil {
-		log.Error("OpenStreamSync on console server ", cfg.Server, err)
-		return err
-	}
-
-	_, err = stream.Write([]byte(cfg.Secret + "\n"))
-	if err != nil {
-		log.Error("Write Secret to console server ", cfg.Server, err)
-		return err
-	}
-	var rMessage map[string]interface{}
-
-	msg, err := io.ReadAll(stream)
-	if err != nil {
-		log.Error("read response console server ", cfg.Server, err)
-		return err
-	}
-	if err = json.Unmarshal(msg, &rMessage); err != nil {
-		log.Error("read response json console server ", cfg.Server, err)
-		return err
-	}
-	if rMessage["code"].(float64) != 0 {
-		log.Error("response from console server ", cfg.Server, " ", rMessage["msg"])
-		return nil
-	} else {
-		log.Info("response from console server ", cfg.Server, " success")
-	}
-	for {
-		s, err := conn.AcceptStream(ctx)
-		if err == nil {
-			go cfg.ReceiveRequest(s)
-		} else {
-			return err
+	if stream := quic.Stream(nil); err == nil {
+		if stream, err = conn.OpenStreamSync(ctx); err == nil {
+			_, err = stream.Write([]byte(cfg.Secret + "\n"))
+			if msg := []byte(nil); err == nil {
+				if msg, err = io.ReadAll(stream); err == nil {
+					var rMessage map[string]interface{}
+					if err = json.Unmarshal(msg, &rMessage); err == nil {
+						if rMessage["code"].(float64) != 0 {
+							log.Error("response from console server ", cfg.Server, " ", rMessage["msg"])
+							return nil
+						} else {
+							log.Info("response from console server ", cfg.Server, " success")
+						}
+					}
+				}
+			}
 		}
 	}
+
+	for err == nil {
+		var s quic.Stream
+		if s, err = conn.AcceptStream(ctx); err == nil {
+			go cfg.ReceiveRequest(s)
+		} else if ctx.Err() == nil {
+			go cfg.Remote(ctx)
+		}
+	}
+
+	if err != nil {
+		log.Error("connect to console server ", cfg.Server, err)
+	}
+	return err
 }
 
 func (cfg *Engine) ReceiveRequest(s quic.Stream) error {
+	defer s.Close()
 	r := bufio.NewReader(s)
 	wr := &myResponseWriter2{Stream: s}
 	str, err := r.ReadString('\n')
@@ -84,7 +79,6 @@ func (cfg *Engine) ReceiveRequest(s quic.Stream) error {
 			if err == nil {
 				h, _ := cfg.mux.Handler(req)
 				h.ServeHTTP(wr, req)
-				return s.Close()
 			}
 		} else {
 			err = errors.New("theres no \\r")
