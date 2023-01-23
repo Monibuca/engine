2c87f7fa0eafd0603af2b1738101c6c2d6ba697d
dexter <178529795@qq.com>
2023-01-23 16:48:59 +0800

    修复flv订阅时的时间戳计算问题

--- subscriber.go
+++ subscriber.go
@@ -230,10 +230,9 @@ func (s *Subscriber) PlayBlock(subType byte) {
 		}
 	case SUBTYPE_FLV:
 		flvHeadCache := make([]byte, 15) //内存复用
-		sendFlvFrame := func(t byte, abs uint32, avcc net.Buffers) {
+		sendFlvFrame := func(t byte, ts uint32, avcc net.Buffers) {
 			flvHeadCache[0] = t
 			result := append(FLVFrame{flvHeadCache[:11]}, avcc...)
-			ts := abs - s.SkipTS
 			dataSize := uint32(util.SizeOfBuffers(avcc))
 			util.PutBE(flvHeadCache[1:4], dataSize)
 			util.PutBE(flvHeadCache[4:7], ts)
