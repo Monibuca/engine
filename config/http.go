9d9a00c83edd76771cbd19d5889c8bbfe2475bc7
Anthony.Liu <anthony.liu@wiwide.com>
2023-05-17 19:02:23 +0800

    解决CVE-2016-2183漏洞

--- config/http.go
+++ config/http.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"crypto/tls"
 	"net/http"
 	"time"
 
@@ -78,6 +79,29 @@ func (config *HTTP) Listen(ctx context.Context) error {
 				WriteTimeout: config.WriteTimeout,
 				IdleTimeout:  config.IdleTimeout,
 				Handler:      config.mux,
+				TLSConfig: &tls.Config{
+					CipherSuites: []uint16{
+						tls.TLS_AES_128_GCM_SHA256,
+						tls.TLS_CHACHA20_POLY1305_SHA256,
+						tls.TLS_AES_256_GCM_SHA384,
+						//tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+						//tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+						//tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+						//tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+						tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+						tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+						tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+						tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+						tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+						tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+						tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+						tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+						tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+						tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+					},
+				},
 			}
 			return server.ListenAndServeTLS(config.CertFile, config.KeyFile)
 		})
